c72b4f074417e3a409585d530f79728a81635f68
rStarboy <rStarboy0809@gmail.com>
2022-01-09 21:44:08 +0300

    change file to zip

---
+++ static/js/159.dbd72013.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[159],{2286:function(a,s){}}]);
+//# sourceMappingURL=159.dbd72013.chunk.js.map
---
+++ static/js/24.93ae660d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[24],{1306:function(a,n,s){},1307:function(a,n,s){}}]);
+//# sourceMappingURL=24.93ae660d.chunk.js.map
